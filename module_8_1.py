9edc66b15873c86761d5373a458a5028c31c806d
IrekGit <irek_mustafin@mail.ru>
2024-11-27 21:41:47 +1000

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        return a + b
+
+    except TypeError:
+        print('Это невозможно сложить, но если надо - сложим!\nПрограммистам все можно!')
+        return f'Вот результат: {a}{b}'
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+print(add_everything_up([1, 2], [3, 4]))
+print(add_everything_up('груша', [3, 4]))
