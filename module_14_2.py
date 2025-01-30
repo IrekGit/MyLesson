f130aaf8ec0ec11ca3c287609d4157d51bb3b4e9
IrekGit <irek_mustafin@mail.ru>
2025-01-30 23:35:37 +1000

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER,
+        balance INTEGER NOT NULL
+    )
+''')
+
+# Заполнение таблицы 10 записями
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"User{i}", f"example{i}@gmail.com", i * 10, 1000))
+
+# Обновление баланса у каждой 2-ой записи начиная с 1-ой
+cursor.execute("UPDATE Users SET balance = 500 WHERE id % 2 = 1")
+
+# Удаление каждой 3-ей записи начиная с 1-ой
+cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
+
+# Выборка всех записей, где возраст не равен 60 и вывод результата в консоль
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60")
+records = cursor.fetchall()
+for record in records:
+    print(f"Имя: {record[0]} | Почта: {record[1]} | Возраст: {record[2]} | Баланс: {record[3]}")
+
+# Удалите из базы данных not_telegram.db запись с id = 6.
+cursor.execute("DELETE FROM Users WHERE id == 6")
+
+# Подсчитать общее количество записей.
+cursor.execute("SELECT COUNT(*) FROM Users")
+print(f"Общее количество записей: {cursor.fetchone()[0]}")
+
+# Посчитать сумму всех балансов.
+cursor.execute("SELECT SUM(balance) FROM Users")
+print(f"Сумма балансов записей: {cursor.fetchone()[0]}")
+
+# Вывести в консоль средний баланс всех пользователей
+cursor.execute("SELECT AVG(balance) FROM Users")
+print(f"Средний баланс записей: {cursor.fetchone()[0]}")
+
+connection.commit()
+connection.close()
